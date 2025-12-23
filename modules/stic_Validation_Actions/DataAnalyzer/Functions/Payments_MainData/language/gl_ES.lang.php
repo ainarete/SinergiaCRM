3ab29838f663443e1b950d68e6d60c5168f9a49c
ainarete <82768990+ainarete@users.noreply.github.com>
2025-12-23 14:01:42 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Payments_MainData/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Payments_MainData/language/gl_ES.lang.php
@@ -21,9 +21,9 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Pagos - Revisión dos datos principais',		// Esta etiqueta é obrigatoria. Indica o nome da función
-    'UPDATED_IBAN' => ' actualizouse para eliminar caracteres non válidos.',
+    'NAME' => 'Pagos - Revisión dos datos principais',		// Esta etiqueta es obligatoria. Indica el nombre de la función
     'UPDATED_IBAN' => ' cun valor incorrecto, actualizouse automaticamente.',
+    'UPDATED_IBAN' => ' con valor erróneo, se ha actualizado automáticamente.',
     'UPDATED_MANDATE' => ' estaba baleiro ou non era válido, actualizouse automaticamente.',
 	'NOVALID_MANDATE' => ' está baleiro ou non válido e non se pode recuperar desde o compromiso de pago.',
 	'INHERIT_MANDATE' => ' estaba baleiro ou non era válido, herdouse desde o compromiso de pago.',
