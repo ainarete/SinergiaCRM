1a2064f362929ffb597dceca9b55b95a8a85847b
ainarete <82768990+ainarete@users.noreply.github.com>
2025-12-23 12:50:10 +0100

    New translations es_es.lang.php (Basque)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Payments_MainData/language/eu_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Payments_MainData/language/eu_ES.lang.php
@@ -22,7 +22,7 @@
  */
 $func_strings = array (
     'NAME' => 'Pagos - Revisión de los datos principales',		// Esta etiqueta es obligatoria. Indica el nombre de la función
-    'UPDATED_IBAN' => ' se ha actualizado para eliminar caracteres no válidos.',
+    'UPDATED_IBAN' => ' con valor erróneo, se ha actualizado automáticamente.',
     'UPDATED_IBAN' => ' con valor erróneo, se ha actualizado automáticamente.',
     'UPDATED_MANDATE' => ' estaba vacío o no era válido, se ha actualizado automáticamente.',
 	'NOVALID_MANDATE' => ' está vacío o no es válido y no se puede recuperar desde el compromiso de pago.',
