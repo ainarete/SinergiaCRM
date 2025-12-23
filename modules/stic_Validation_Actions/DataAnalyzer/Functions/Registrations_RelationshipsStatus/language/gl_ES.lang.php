015c4c668bdd2b97e4642deddc28593c53fdf2d9
ainarete <82768990+ainarete@users.noreply.github.com>
2025-12-23 12:50:16 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Registrations_RelationshipsStatus/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Registrations_RelationshipsStatus/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Inscricións - Revisión das relacións',		// Esta etiqueta é obrigatoria. Indica o nome da función
+    'NAME' => 'Inscricións - Revisión das relacións',		// Esta etiqueta es obligatoria. Indica el nombre de la función
     'NO_LINK' => 'Non ten ningunha persoa, organización ou interesado vinculado.',
     'NO_EVENT' => 'Non ten ningún evento vinculado.',
 );
