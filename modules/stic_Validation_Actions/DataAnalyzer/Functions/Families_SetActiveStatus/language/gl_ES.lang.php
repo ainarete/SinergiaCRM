0b2050c79a94ccdf9ec7001ba1cc06d8dfe98a84
ainarete <82768990+ainarete@users.noreply.github.com>
2025-12-23 14:01:25 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Families_SetActiveStatus/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Families_SetActiveStatus/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array(
-    'NAME' => 'Unidades familiares - Cálculo de rexistro activo/inactivo', // Esta etiqueta é obrigatoria. Indica o nombre da función
+    'NAME' => 'Unidades familiares - Cálculo de rexistro activo/inactivo', // Esta etiqueta es obligatoria. Indica el nombre de la función
     'NO_ROWS' => 'Todas as familias teñen o estado activo/inactivo correcto.',
     'UPDATED' => 'Actualizouse o campo Activo.',
 );
