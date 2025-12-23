0347b704bf8518ac223f3fc16977d62040068bb3
ainarete <82768990+ainarete@users.noreply.github.com>
2025-12-23 12:51:04 +0100

    New translations es_es.sticlang.php (Galician)

--- custom/Extension/modules/Home/Ext/Language/gl_ES.SticLang.php
+++ custom/Extension/modules/Home/Ext/Language/gl_ES.SticLang.php
@@ -34,6 +34,5 @@ $mod_strings['LBL_ABOUT_SINERGIACRM_KREPORTER'] = 'Kreporter de Christian Knoll'
 $mod_strings['LBL_ABOUT_SINERGIACRM_MAIL_MERGE'] = 'Mail Merge Reports de Izertis';
 $mod_strings['LBL_ABOUT_SINERGIACRM_OTHER'] = 'Outras librarías de código aberto que se atopan nas carpetas <i>/vendor</i> e <i>/SticInclude/vendor</i>. A información de licenza destas librarías pódese atopar nos seus propios ficheiros.';
 
-
 // SinergiaDA
 $mod_strings['LBL_STIC_SINERGIADA_NOT_ALLOWED'] = 'No tiene habilitado el acceso a SinergiaDA. Contacte con el administrador.'; 
