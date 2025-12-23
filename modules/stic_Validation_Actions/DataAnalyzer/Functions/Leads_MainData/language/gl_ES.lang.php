aa24e65f7f3290e67c2502480b4dc29085579dfc
ainarete <82768990+ainarete@users.noreply.github.com>
2025-12-23 12:50:01 +0100

    New translations es_es.lang.php (Galician)

--- modules/stic_Validation_Actions/DataAnalyzer/Functions/Leads_MainData/language/gl_ES.lang.php
+++ modules/stic_Validation_Actions/DataAnalyzer/Functions/Leads_MainData/language/gl_ES.lang.php
@@ -21,7 +21,7 @@
  * You can contact SinergiaTIC Association at email address info@sinergiacrm.org.
  */
 $func_strings = array (
-    'NAME' => 'Interesados - Revisión dos datos principais',		// Esta etiqueta é obrigatoria. Indica o nome da función
+    'NAME' => 'Interesados - Revisión dos datos principais',		// Esta etiqueta es obligatoria. Indica el nombre de la función
     'NO_VALID_CIF' => 'Non é un CIF válido.',
 	'NO_VALID_NIF_NIE' => 'Non é un NIF/NIE válido.',
 	'UPDATED' => 'Limpouse o campo para eliminar caracteres non válidos.',
